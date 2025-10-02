230b2cb0743fa17dd46d7ca78394b72a87484789
Michael Medvedev <mmedvedev20@gmail.com>
2025-10-01 20:40:55 -0400

    completed cli for reverse engineering client code from source maps

--- cli.js
+++ cli.js
@@ -3,10 +3,11 @@
 import * as cheerio from "cheerio"
 import {exec} from "child_process"
 import {promisify} from "util"
+import {mkdirSync, writeFileSync} from "fs"
+import path from "path"
 import fs from "fs"
 var args = process.argv.slice(2)
 var BASE_URL = args[args.length-1];
-console.log(args)
 
 function log(msg) {
     console.log(msg + "...")
@@ -23,37 +24,17 @@ if (args.length === 0) {
     process.exit(1)
 }
 
-for (const [index, cmd] of args.entries()) {
+for (const cmd of args) {
     switch (cmd) {
         case "--help":
-            console.log(`Usage: sourcemap [command] [options...]
-    
-    Commands:
-        inspect <url> Inspect a url for sourcemaps using curl`)
-            break;
-        case "--manual":
-            console.log("Help Menu")
+            displayMenu()
             break;
         case "inspect":
-            let html, url;
-            
-            try {
-                url = URL.parse(args[args.length-1])
-                if (!url) throw Error("ERROR PARSING URL")
-                html = await inspect(url)
-                const sources = await parseIndexHTML(html)
-                const sourceMaps = await findSourceMaps(sources)
-                console.log("All Done!")
-                console.log(`Source maps
-                ${JSON.stringify(sourceMaps)}`)
-                process.exit(0)
-
-            } catch (e) {
-                console.error(e)
-                process.exit(1)
-            }
-
-            break;
+            await findMaps()
+            process.exit(0)
+        case "generate":
+            await generateClient()
+            process.exit(0)
         default:
             console.error("Invalid command: try 'sourcemap --help' or 'sourcemap --manual' for more information")
             process.exit(1)
@@ -61,7 +42,25 @@ for (const [index, cmd] of args.entries()) {
     }
 }
 
-async function inspect(url) {
+async function findMaps(){
+    let html, url;
+            try {
+                url = URL.parse(args[args.length-1])
+                if (!url) throw Error("ERROR PARSING URL")
+                html = await executeCurlCommand(url)
+                const sources = await parseIndexHTML(html)
+                const sourceMapUrls = await findSourceMaps(sources)
+                console.log("All Done!")
+                console.log("Source Maps Discovered: ")
+                console.log(sourceMapUrls)
+                return sourceMapUrls
+            } catch (e) {
+                console.error(e)
+                process.exit(1)
+            }
+}
+
+async function executeCurlCommand(url) {
     try {
         let curlOptions = ""
         for(let x of args.slice(1, -1)){
@@ -82,7 +81,7 @@ async function inspect(url) {
         return stdout
         
     } catch (e) {
-        console.error('❌ Curl execution failed:')
+        console.error('Curl execution failed:')
         console.error('Error message:', e.message)
         console.error('Error code:', e.code)
         console.error('Command attempted:', `curl -s ${curlOptions}"${url}"`)
@@ -135,7 +134,7 @@ async function parseIndexHTML(html) {
 }
 
 async function findSourceMaps(sources) {
-    const sourceMaps = [];
+    const sourceMapUrls = [];
     log("Parsing assets for sourcemaps")
     for (const asset of [...sources.js, ...sources.css]) {
         try{
@@ -145,7 +144,7 @@ async function findSourceMaps(sources) {
             const result = await res.text()
             if(result.includes("sourceMappingURL=")) {
                 console.log(`Found Source Map at ${asset}`)
-                sourceMaps.push(`${BASE_URL}${asset}.map`)
+                sourceMapUrls.push(`${BASE_URL}${asset}.map`)
             }
         }
         catch(e){
@@ -153,5 +152,76 @@ async function findSourceMaps(sources) {
             process.exit(1)
         }        
     }
-    return sourceMaps
+    return sourceMapUrls
+}
+
+
+function displayMenu(){
+    console.log(`Usage: sourcemap [command] [options...]
+    
+        Commands:
+            inspect <url> Inspect a url for sourcemaps using curl`)
+}
+
+async function generateClient(){
+    const sourceMapURLs = await findMaps();
+    log("Generating Code")
+    const sourceMap = await fetchSourceMap(sourceMapURLs)
+    generateCode(sourceMap)
+}
+
+
+
+
+/**
+ * the Source Map structure
+ * [
+    'version',
+    'file',
+    'sources',
+    'sourceRoot',
+    'sourcesContent',
+    'names',
+    'mappings',
+    'debug_id'
+  ]
+ */
+
+
+
+async function fetchSourceMap(sourceMapURLs){
+    const promises = sourceMapURLs.map(url => fetch(url))
+    const [jsData, cssData] = await Promise.all(promises)
+    console.log(sourceMapURLs)
+    log("done")
+    return { 
+        js: await jsData.json(),
+        css: await cssData.json() 
+    }
+}
+
+function cleanPath(path){
+    if(typeof path != "string") throw new Error("Error path must be a string to clean")
+    return path.replace(/(\.\.\/)+/g, "")
+}
+
+function generateDirectory(source, index, sourcesContent){
+    if(typeof source != "string") throw new Error("Error source must be a string")
+    let cleanedSource = cleanPath(source)
+    let directory = path.dirname(cleanedSource)
+    let file = path.basename(cleanedSource)
+    mkdirSync(path.join("temp1", directory), {recursive: true})
+    writeFileSync(path.join("temp1", directory, file), sourcesContent[index] ?? "null")
+}
+
+function generateCode(sourceMap){
+    const jsMap = sourceMap.js
+    const jsCode = jsMap.sourcesContent
+    const cssMap = sourceMap.css
+    const cssCode = cssMap.sourcesContent
+
+
+    jsMap.sources.forEach((source, index) => generateDirectory(source, index, jsCode))
+    cssMap.sources.forEach((source, index) => generateDirectory(source, index, cssCode))
+
 }
